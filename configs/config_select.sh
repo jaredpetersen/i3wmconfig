e55f42254526789fd201e2541f54d9390a5bbde2
Jared Petersen <jpetersen11@wou.edu>
2015-02-12 19:20:50 -0800

    Changed the Status Bar Icons and Renamed the Configs folder

--- configs/config_select.sh
+++ configs/config_select.sh
@@ -10,19 +10,19 @@ rm ~/.i3/config
 
 if [ "$randomInt" = "0" ]; then
 	# Yellow Config
-	cp ~/Configs/config0 ~/.i3/config
+	cp ~/configs/config0 ~/.i3/config
 	xsetroot -solid "#f9d215"
 elif [ "$randomInt" = "1" ]; then
 	# Green Config
-	cp ~/Configs/config1 ~/.i3/config
+	cp ~/configs/config1 ~/.i3/config
 	xsetroot -solid "#13a43b"
 elif [ "$randomInt" = "2" ]; then
 # Red Config
-	cp ~/Configs/config2 ~/.i3/config
+	cp ~/configs/config2 ~/.i3/config
 	xsetroot -solid "#cb0810"
 else
 # Blue Config
-	cp ~/Configs/config3 ~/.i3/config
+	cp ~/configs/config3 ~/.i3/config
 	xsetroot -solid "#738baf"
 fi
 
